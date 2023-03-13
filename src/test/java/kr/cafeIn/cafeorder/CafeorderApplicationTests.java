6c7ee69c973581d84962f9b1ba10f004c2552a1d
kimmangi <kmg0718@gmail.com>
2023-03-13 23:16:42 +0900

    initialize

---
+++ src/test/java/kr/cafeIn/cafeorder/CafeorderApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.cafeIn.cafeorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

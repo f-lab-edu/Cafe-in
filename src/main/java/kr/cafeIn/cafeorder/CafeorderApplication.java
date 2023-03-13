994558bb570d943f5d9e5a84577861fd1283f9f2
kimmangi <kmg0718@gmail.com>
2023-03-13 23:16:42 +0900

    initialize

---
+++ src/main/java/kr/cafeIn/cafeorder/CafeorderApplication.java
@@ -0,0 +1,13 @@
+package kr.cafeIn.cafeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeorderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafeorderApplication.class, args);
+    }
+
+}
